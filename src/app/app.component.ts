4fc682cd355ef02b975cd428677757598e50c2ff
Samys Molina <samyjes4@gmail.com>
2025-01-14 15:03:41 -0400

    create componente report and config pdfmake, add pdf.ts and pdfmake.d.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReportComponent } from "./report/report.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/interfaces/report.interface.ts
@@ -0,0 +1,5 @@
+export interface Report {
+    date: string,
+    ReportNo: number,
+    description: string,
+}
